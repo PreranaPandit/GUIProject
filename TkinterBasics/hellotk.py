be5fb00b088461fa9719b8d8919d2de8989283e8
PreranaPandit <preranapandit07@gmail.com>
2021-03-15 15:19:35 +0545

    Initial commit

---
+++ TkinterBasics/hellotk.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+root = Tk()
+
+#creating a label widget
+myLabel = Label(root, text = "Tkinter Program Beginning")
+
+#Shoving it onto the sreen
+myLabel.pack()
+
+root.mainloop()
+
