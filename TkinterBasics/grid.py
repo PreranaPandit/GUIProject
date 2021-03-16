fb85a7f04744973c4537409fd0908067b440fde3
PreranaPandit <preranapandit07@gmail.com>
2021-03-16 14:43:23 +0545

    grid forming with row and column

--- TkinterBasics/grid.py
+++ TkinterBasics/grid.py
@@ -1,1 +1,18 @@
-a = 10
+from tkinter import *
+
+root = Tk()
+
+#creating a label widget
+myLabel1 = Label(root, text="Tkinter Program Beginning")
+myLabel2 = Label(root, text="I am Prerana Pandit")
+myLabel3 = Label(root, text="jhghfgdfgfd+empty")
+
+#Shoving it onto the sreen based upon x-axis and y-axis that does not move having a constant position
+myLabel1.grid(row=0, column=0)
+myLabel2.grid(row=1, column=5)
+myLabel3.grid(row=1, column=1)
+'''
+# the changes of column of label2 does not changes as there are not any texts to assign
+'''
+
+root.mainloop()
