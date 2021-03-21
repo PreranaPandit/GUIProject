c5c87709bac99917c3e4cde3e1776d7bf5b5ebbe
PreranaPandit <preranapandit07@gmail.com>
2021-03-21 09:35:28 +0545

    icon images and button exit

---
+++ IconImages/images.py
@@ -0,0 +1,20 @@
+from tkinter import *
+#from tkinter import Image
+
+root = Tk()
+
+# title
+root.title('Images Insertion')
+
+# icon images
+# png icons does not support somoetimes
+root.iconbitmap('E:/images/global.ico')
+
+'''
+filen = r'E:/images/medicine.png'
+img = Image.open(filen)
+img.save('icon.ico',format = 'ICO', sizes=[(32,32)])
+'''
+
+
+root.mainloop()
