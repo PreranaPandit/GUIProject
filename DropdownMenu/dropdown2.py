3b86ce837c1094d210181cb162c4aa139d04c8b7
PreranaPandit <preranapandit07@gmail.com>
2021-03-24 15:53:35 +0545

    dropdown menu completion using list

---
+++ DropdownMenu/dropdown2.py
@@ -0,0 +1,35 @@
+from tkinter import *
+from PIL import ImageTk, Image
+from tkinter import messagebox
+from tkinter import filedialog
+
+root = Tk()
+root.title('Message Box')
+root.iconbitmap('E:/images/global.ico')
+
+
+def show():
+    myLabel = Label(root, text=clicked.get()).pack()
+
+# making options into list
+options= [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday"
+]
+
+clicked = StringVar()
+clicked.set("Monday")
+
+drop = OptionMenu(root, clicked,*options)
+drop.pack()
+
+myButton = Button(root, text="Show selection", command=show).pack()
+
+
+
+
+mainloop()
+
