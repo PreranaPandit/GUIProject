845cb845bc942b89b0488c5d5a6ac10c9c69baae
PreranaPandit <preranapandit07@gmail.com>
2021-03-24 14:26:54 +0545

    slider process ongoing

---
+++ Sliders/slider.py
@@ -0,0 +1,16 @@
+from tkinter import *
+from PIL import ImageTk, Image
+from tkinter import messagebox
+from tkinter import filedialog
+
+root = Tk()
+root.title('Message Box')
+root.iconbitmap('E:/images/global.ico')
+
+vertical = Scale(root, from_=0, to=200)
+vertical.pack()
+
+
+
+mainloop()
+
