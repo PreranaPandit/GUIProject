ea388a280ea0ca8dec33f4af043dba71a2b2d2a6
PreranaPandit <preranapandit07@gmail.com>
2021-03-24 14:09:02 +0545

    dialog box completion

---
+++ DialogBox/dialog_box2.py
@@ -0,0 +1,26 @@
+from tkinter import *
+from PIL import ImageTk, Image
+from tkinter import messagebox
+from tkinter import filedialog
+
+root = Tk()
+root.title('Message Box')
+root.iconbitmap('E:/images/global.ico')
+
+
+def open():
+    global my_image
+    root.filename = filedialog.askopenfilename(initialdir="/Downloads", title="Select a file",
+                                               filetypes=(("png files", "*.png"), ("all files", "*.*")))
+
+    # sets the location of the selected image in the label
+    my_label = Label(root, text=root.filename).pack()
+    # image also gets displayed
+    my_image = ImageTk.PhotoImage(Image.open(root.filename))
+    my_image_label = Label(image=my_image).pack()
+
+
+my_btn = Button(root, text="Open File", command=open).pack()
+
+mainloop()
+
