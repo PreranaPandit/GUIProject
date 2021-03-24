0f1121e1fd0362c70321578823258352088fdd83
PreranaPandit <preranapandit07@gmail.com>
2021-03-24 15:46:53 +0545

    dropdown menu completion

---
+++ DropdownMenu/dropdown1.py
@@ -0,0 +1,25 @@
+from tkinter import *
+from PIL import ImageTk, Image
+from tkinter import messagebox
+from tkinter import filedialog
+
+root = Tk()
+root.title('Message Box')
+root.iconbitmap('E:/images/global.ico')
+
+def show():
+    myLabel = Label(root, text=clicked.get()).pack()
+
+clicked = StringVar()
+clicked.set("Monday")
+
+drop = OptionMenu(root, clicked,"Moday","Tuesday","Wednesday","Thursday","Friday")
+drop.pack()
+
+myButton = Button(root, text="Show selection", command=show).pack()
+
+
+
+
+mainloop()
+
