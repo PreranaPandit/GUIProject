a8fb8117ef9c783e80a135717291afd63fa8ab51
PreranaPandit <preranapandit07@gmail.com>
2021-03-24 13:12:44 +0545

    window created and closed

---
+++ NewWindow/window3.py
@@ -0,0 +1,26 @@
+from tkinter import *
+from PIL import ImageTk, Image
+from tkinter import messagebox
+
+root = Tk()
+root.title('Message Box')
+root.iconbitmap('E:/images/global.ico')
+
+#Creates new window
+
+def open():
+    global my_img
+    top = Toplevel()
+    top.title('Check message box')
+    top.iconbitmap('E:/images/global.ico')
+    my_img = ImageTk.PhotoImage(Image.open("E:/images/smartphone.png"))
+    my_label = Label(top, image=my_img).pack()
+    # closing the window using button.destroy
+
+    btn2 = Button(top, text="Close Window", command = top.destroy).pack()
+
+btn = Button(root, text="Open", command=open).pack()
+
+
+mainloop()
+
